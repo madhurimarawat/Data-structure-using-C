5b1f6a98011aca9859149802761598d32912c4da
madhurimarawat <105432776+madhurimarawat@users.noreply.github.com>
2022-08-04 13:10:34 +0530

    04-08-2022_bubblesort push

---
+++ bubblesort-code.c
@@ -0,0 +1,248 @@
+#include<stdio.h>
+#include<stdlib.h>
+/* Write the function for Bubble sort using C.*/
+
+	void bubbleSort (int *S,  int length) {
+	 int isSorted = 0;
+	while(!isSorted)
+   	{
+		isSorted = 1;
+		for(int i = 0; i<length-1; i++)
+      	{
+		     if(S[i] > S[i+1])
+           	     {
+			int temp = S[i];
+			S[i] = S[i+1];
+     	       		S[i+1] = temp;
+            		isSorted = 0;            }
+      	}
+      	length--;
+}
+} 
+void main(){
+int *arr,n,i;
+printf("Enter number of elements: ");
+scanf("%d",&n);
+arr= (int*)calloc(n,sizeof(int));
+for(i=0;i<n;i++){
+printf("Enter element %d: ",i+1);
+scanf("%d",&arr[i]);
+}
+printf("Sorted array is: ");
+ bubbleSort (arr,n);
+for(i=0;i<n;i++){
+           	   printf("%d\t ",*(arr+i));
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
