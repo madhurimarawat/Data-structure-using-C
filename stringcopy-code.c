f552d8b60793292673a3da03a64439d4cfe4c8b1
madhurimarawat <105432776+madhurimarawat@users.noreply.github.com>
2022-08-05 15:00:14 +0530

    05-08-2022
    String copy using user defined functions.

---
+++ stringcopy-code.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(){
+char i,str1[100],str2[100];
+ printf("Enter a string:");
+gets(str1);
+for(i=0;str1[i]!='\0';++i){
+str2[i]=str1[i];
+}
+str2[i]='\0';
+ printf("Entered string is %s\nCopied str is %s",str1,str2);
+}
