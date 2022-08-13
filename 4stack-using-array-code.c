880fcb5f5b54f7364949f97437adb7c6df49a756
madhurimarawat <105432776+madhurimarawat@users.noreply.github.com>
2022-08-13 20:31:17 +0530

    13-08-2022(lab 2 program 4)

---
+++ 4stack-using-array-code.c
@@ -0,0 +1,85 @@
+#include<stdio.h>
+#include<stdlib.h>
+/*4. Implement Stack data structure and associated operations using arrays.
+Design a menu for using the operations.*/
+void print(int *a,int n){
+printf("Stack is:\t");
+for(int i=0;i<n;i++){
+           	   printf("%d\t ",*(a+i));
+} }
+void isempty(int *arr,int n){
+int e=0;
+for(int i=0;i<n;i++){
+if(arr[i]!=0){e++;}}
+if(e!=0){
+printf("Stack is Not Empty");}
+else {printf("Stack is Empty");}
+}
+void isfull(int *arr,int n){
+int e=0;
+for(int i=0;i<n;i++){
+if(arr[i]==0){e++;}}
+if(e==0){
+printf("Stack is  Full");}
+else {printf("Stack is Not Full");}
+}
+void push(int *arr,int n,int e){
+arr=realloc(arr,n+1);
+arr[n]=e;
+print(arr,n+1);}
+void pop(int *arr,int n){
+printf("%d\n",arr[n-1]);
+arr=realloc(arr,n-1);
+print(arr,n-1);}
+void peek(int *arr,int n){
+printf("%d",arr[n-1]);
+   }
+void size(int *arr){
+printf("The size of stack is:%d",sizeof(arr));}
+int choice(int *arr,int n){
+int c,e;
+printf("Enter 1 for Push, 2 for Pop, 3 for Peek , 4 for Size,5 for is empty or 6 for is full: ");
+scanf("%d",&c);
+switch(c){
+case 1: 
+printf("Enter element to push:");
+scanf("%d",&e);
+push(arr,n,e);
+return n+1; break;
+case 2:
+pop(arr,n);
+return n-1; break;
+case 3:
+peek(arr,n);
+return n; break;
+case 4:
+size(arr); return n; break;
+case 5:
+isempty(arr,n); return n; break;
+case 6:
+isfull(arr,n); return n; break;
+default:
+printf("Invalid case!!"); return -1;
+} }
+
+int main(){
+ int *arr; 
+int n,c,test=1,e;
+printf("Enter number of members in stack:");
+scanf("%d",&n);
+arr=calloc(n,sizeof(int));
+for(int i=0;i<n;i++){
+printf("Enter element %d: ",i+1);
+scanf("%d",&arr[i]);
+}
+int nsize=choice(arr,n);
+while(test==1){
+printf("\nIf you want to repeat Enter 1 else Enter 2:");
+scanf("%d",&test);
+if(test==1){
+int e=choice(arr,nsize);
+if(e==n+1){nsize++;}else if(e==(-1)){break;} 
+else if(e==n-1){nsize--;}else if(e==n){nsize=n;}}
+else{
+free(arr); }}
+return 0; }
