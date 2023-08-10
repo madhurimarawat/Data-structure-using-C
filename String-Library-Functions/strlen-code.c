5cd3c921b367abfaaff64ab6483f9a8034207075
Madhurima Rawat <105432776+madhurimarawat@users.noreply.github.com>
2023-08-10 12:22:59 +0530

    Add files via upload

---
+++ String-Library-Functions/strlen-code.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+// For string functions
+#include<string.h>
+
+// Code for Finding length of string
+
+// Main function
+
+void main(){
+
+// Declaring String
+char str[100];
+
+int l;
+
+// Taking Input
+ printf("Enter a string:");
+gets(str);
+
+// Finding Length of string
+l=strlen(str);
+
+ printf("Entered string  length is: %d",l);
+}
